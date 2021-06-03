938500defef20b25a6bbf24768e010d82cf9b613
Brady Roscher <bradyroscher@yahoo.com>
2021-06-03 15:05:08 -0400

    added backend for artist comment

---
+++ models/artistComments.js
@@ -0,0 +1,6 @@
+const ArtistComment = new Schema({
+  text: { type: String, required: true },
+  artistId: { type: String, required: true }
+})
+
+module.exports = mongoose.model('artistcomments', ArtistComment)
