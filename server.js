cf3943d33b042f3092551c5333c6172d87687e94
Brady Roscher <bradyroscher@yahoo.com>
2021-06-03 16:56:49 -0400

    initializing

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const db = require('./db')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
+const path = require('path')
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', routes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
